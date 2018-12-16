f51de62619aba8917de70fe4c86ad67d30b09684
yambal <jy@yambal.net>
2018-12-16 23:16:41 +0900

    Gem, Bundle, Gememfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
           page: {
               command: [
                 'asciidoctor -b html5 -o docs/index.html index.asciidoc',
+                'asciidoctor -b html5 -o docs/Gem.html Gem.asciidoc',
                 'asciidoctor -b html5 -o docs/Grape.html Grape.asciidoc',
                 'asciidoctor -b html5 -o docs/Rack.html Rack.asciidoc',
                 'asciidoctor -b html5 -o docs/Asciidoc.html Asciidoc.asciidoc',
@@ -13,6 +14,7 @@ module.exports = function(grunt) {
             pdf: {
                 command: [
                   'asciidoctor-pdf -r asciidoctor-pdf-cjk ./index.asciidoc -o docs/pdf/index.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Gem.asciidoc -o docs/pdf/Gem.pdf',
                   'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Rack.asciidoc -o docs/pdf/Rack.pdf',
                   'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Asciidoc.asciidoc -o docs/pdf/Asciidoc.pdf'
                 ].join('&&')
