a62f2488655a5e2e70b146f810835615a8a62176
June YAMAMOTO <june@yambal.net>
2018-12-19 13:19:15 +0900

    RoR on Haroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,19 +9,21 @@ module.exports = function(grunt) {
                 'asciidoctor -b html5 -o docs/Gem.html Gem.asciidoc',
                 'asciidoctor -b html5 -o docs/Grape.html Grape.asciidoc',
                 'asciidoctor -b html5 -o docs/RubyApplication.html RubyApplication.asciidoc',
+                'asciidoctor -b html5 -o docs/HerokuRoR.html HerokuRoR.asciidoc',
                 'asciidoctor -b html5 -o docs/Rack.html Rack.asciidoc',
                 'asciidoctor -b html5 -o docs/Asciidoc.html Asciidoc.asciidoc',
               ].join('&&')
           },
             pdf: {
                 command: [
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./index.asciidoc -o docs/pdf/index.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./index.asciidoc -o docs/pdf/index.pdf',
                   'asciidoctor-pdf -r asciidoctor-pdf-cjk ./RubyForDummies.asciidoc -o docs/pdf/RubyForDummies.pdf',
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Gem.asciidoc -o docs/pdf/Gem.pdf',
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Grape.asciidoc -o docs/pdf/Grape.pdf',
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./RubyApplication.asciidoc -o docs/pdf/RubyApplication.pdf',
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Rack.asciidoc -o docs/pdf/Rack.pdf',
-                  //'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Asciidoc.asciidoc -o docs/pdf/Asciidoc.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Gem.asciidoc -o docs/pdf/Gem.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Grape.asciidoc -o docs/pdf/Grape.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./RubyApplication.asciidoc -o docs/pdf/RubyApplication.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./HerokuRoR.asciidoc -o docs/pdf/HerokuRoR.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Rack.asciidoc -o docs/pdf/Rack.pdf',
+                  'asciidoctor-pdf -r asciidoctor-pdf-cjk ./Asciidoc.asciidoc -o docs/pdf/Asciidoc.pdf',
                 ].join('&&')
             }
         }
